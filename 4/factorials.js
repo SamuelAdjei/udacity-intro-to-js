94333f7d11799af9f8b3f681122352b8188d5db4
Samuel Adjei <samueladjei@Samuels-MacBook-Pro.local>
2019-08-09 19:18:38 +0100

    new code

---
+++ 4/factorials.js
@@ -0,0 +1,22 @@
+/*
+
+Directions:
+Write a for (note: not a function) loop that prints out the factorial of the number 12:
+
+A factorial is calculated by multiplying a number by all the numbers below it. For instance, 3! or "3 factorial" is 3 * 2 * 1 = 6
+
+3! = 3 * 2 * 1 = 6 3!=3∗2∗1=6
+4! = 4 * 3 * 2 * 1 = 24 4!=4∗3∗2∗1=24
+5! = 5 * 4 * 3 * 2 * 1 = 120 5!=5∗4∗3∗2∗1=120
+
+Save your final answer in a variable called solution and print it to the console.
+
+*/
+
+let solution = 1;
+
+for (let i = 1; i <= 12; i++) {
+  solution *= i;
+}
+
+console.log(solution);
