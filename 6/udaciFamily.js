fe770bd6f0e5f48e424b1b11eb1929ea05897a76
Samuel Adjei <samueladjei@Samuels-MacBook-Pro.local>
2019-08-12 22:46:49 +0100

    new code

---
+++ 6/udaciFamily.js
@@ -0,0 +1,9 @@
+/*
+Create an array called udaciFamily and add "Julia", "James", and your name to the array. 
+Then, print the udaciFamily to the console using console.log.
+
+*/
+
+var udaciFamily = ["Julia", "James", "Samuel"];
+
+console.log(udaciFamily);
